ff49dfb32514cc57e5768a809b5b44abed69f71e
Timahi <timahilesage@gmail.com>
2024-02-15 15:52:10 +0100

    transition

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,11 +1,18 @@
-import rss, { pagesGlobToRssItems } from "@astrojs/rss";
+import rss from "@astrojs/rss";
+import { getCollection } from "astro:content";
 
 export async function GET() {
+	const posts = await getCollection("posts");
 	return rss({
 		title: "Astro Learner | Blog",
 		description: "Mon voyage d'apprentissage d'Astro",
 		site: "https://astro-tuto-7c6b88.netlify.app",
-		items: await pagesGlobToRssItems(import.meta.glob("./**/*.md")),
+		items: posts.map((post) => ({
+			title: post.data.title,
+			pubDate: post.data.pubDate,
+			description: post.data.description,
+			link: `/posts/${post.slug}/`,
+		})),
 		customData: `<language>fr-fr</language>`,
 	});
 }
